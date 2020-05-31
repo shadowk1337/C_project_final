6cf015416a268067b4d620dac51be60f13a4fe10
shadowk1337 <56293523+shadowk1337@users.noreply.github.com>
2020-05-31 20:28:54 +0300

    Update log.h

--- compl2/log.h
+++ compl2/log.h
@@ -7,8 +7,12 @@
 #include <time.h>
 #define AR_SIZE 200
 
-void print_log(char *login, char *func_name, char *parameter){
+extern char login[TSIZE];
+
+void print_log(char *login, char *func_name, char *param){
     FILE *f;
+    char parameter[TSIZE];
+    strcpy(parameter, param);
     char str_to_print[AR_SIZE];
     struct tm *loc_time;
     char time_ar[AR_SIZE];
@@ -34,4 +38,58 @@ void print_log(char *login, char *func_name, char *parameter){
     fclose(f);
 }
 
+FILE *open_file(char *filename, char *func){
+    FILE *f;
+    if ((f = fopen(filename, func)) == NULL){
+        fprintf(stderr, "Не удалось открыть файл %s", filename);
+        print_log(login, "EEE Не удалось открыть файл", "\0");
+        exit(EXIT_FAILURE);
+    };
+    return f;
+}
+
+void students_menu(int vip){
+    puts("\n**** STUDENTS MENU ****\n"
+         "Выберите операцию:\n"
+         " 1) Добавить студента\n"
+         " 2) Удалить студента по номеру зачетной книжки\n"
+         " 3) Бэкап\n"
+         " 4) Восстановить базу из файла бэкапа\n"
+         " 5) Поиск по фамилии\n"
+         " 6) Поиск по номеру зачетки\n"
+         " 7) Завершить работу программы");
+    if (vip == 1)
+        puts("0)Вернуться в меню выбора");
+    printf("> ");
+}
+
+void books_menu(int vip){
+    puts("\n**** BOOKS MENU ****\n"
+         "Выберите операцию:\n"
+         " 1) Добавить книгу\n"
+         " 2) Удалить книгу\n"
+         " 3) Просмотр информации о книге\n"
+         " 4) Вывести таблицу книг\n"
+         " 5) Поиск по ISBN\n"
+         " 6) Редакитровать книгу\n"
+         " 7) Выдача книги\n"
+         " 8) Сдача книги\n"
+         " 9) Изменить количество книг\n"
+         "10) Бэкап\n"
+         "11) Восстановление\n"
+         "12) Поиск по автору\n"
+         "13) Завершить программу");
+    if (vip == 2)
+        puts("0)Вернуться в меню выбора");
+    printf("> ");
+}
+
+int no_data (int size){
+    if (size == 0){
+        printf("В читаемом файле нету данных\n--Завершение функции--\n");
+        print_log(login, "WWW В читаемом файле нету данных —Завершение функции--", "\0");
+        return 1;
+    }
+    return 0;
+}
 #endif
